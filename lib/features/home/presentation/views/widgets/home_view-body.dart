71bcde0e2630433b0ef152f08d160011fe95edc5
ahmedabdelhaliem <144239916+ahmedabdelhaliem@users.noreply.github.com>
2024-05-09 05:51:43 -0700

    create style file

--- lib/features/home/presentation/views/widgets/home_view-body.dart
+++ lib/features/home/presentation/views/widgets/home_view-body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_mvvm/core/utils/styles.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/CustomAppar.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/feature_list_view.dart';
 import 'package:flutter/material.dart';
@@ -8,11 +9,22 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(
-        children: [
-          CustomAppBar(),
-          FeatureListView(),
-        ],
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomAppBar(),
+            FeatureListView(),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Best Seller',
+              style: Styles.titleMedium,
+            )
+          ],
+        ),
       ),
     );
   }
