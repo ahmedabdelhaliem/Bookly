6d25d45ad879769334a95fc3ff5652e9895ad633
ahmedabdelhaliem <144239916+ahmedabdelhaliem@users.noreply.github.com>
2024-05-11 05:04:44 -0700

    create app styles

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 // ignore: constant_identifier_names
 const Color kPrimary = Color(0xff100B20);
 const Duration kTransitionDuration = Duration(milliseconds: 2);
+const kGTSectraFine = 'GTSectraFine';
