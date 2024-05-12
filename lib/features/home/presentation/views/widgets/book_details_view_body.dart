81eaab449d9af2fab30022ae34389945b9a25e39
ahmedabdelhaliem <144239916+ahmedabdelhaliem@users.noreply.github.com>
2024-05-12 05:32:16 -0700

    add rating to book detalis

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,8 +1,10 @@
 import 'package:bookly_mvvm/core/utils/styles.dart';
+import 'package:bookly_mvvm/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/custom_book_details_appbar.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -29,10 +31,19 @@ class BookDetailsViewBody extends StatelessWidget {
           const SizedBox(
             height: 6,
           ),
-          Text(
-            'Rudyard Kipling',
-            style: Styles.textStyle18.copyWith(
-                fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                  fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+            ),
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
           )
         ],
       ),
