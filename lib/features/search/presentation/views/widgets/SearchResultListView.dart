4fcce7341395340b76217fd25834e42f39166df3
ahmedabdelhaliem <144239916+ahmedabdelhaliem@users.noreply.github.com>
2024-05-14 03:26:33 -0700

    cretate book model

---
+++ lib/features/search/presentation/views/widgets/SearchResultListView.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_mvvm/features/home/presentation/views/widgets/best_seller_list_view-item.dart';
+import 'package:flutter/cupertino.dart';
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (BuildContext context, int index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(),
+        );
+      },
+    );
+  }
+}
--- lib/features/search/presentation/views/widgets/Search_view_body.dart
+++ lib/features/search/presentation/views/widgets/Search_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_mvvm/core/utils/styles.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/best_seller_list_view-item.dart';
+import 'package:bookly_mvvm/features/search/presentation/views/widgets/SearchResultListView.dart';
 import 'package:bookly_mvvm/features/search/presentation/views/widgets/custom_text_search_field.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,43 +11,28 @@ class SearchViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        CustomTextField(),
-        SizedBox(
-          height: 16,
-        ),
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            'search result',
-            style: Styles.textStyle18,
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          CustomTextField(),
+          SizedBox(
+            height: 16,
           ),
-        ),
-        SizedBox(
-          height: 16,
-        ),
-        Expanded(child: SearchResultListView())
-      ],
-    );
-  }
-}
-
-class SearchResultListView extends StatelessWidget {
-  const SearchResultListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: BookListViewItem(),
-        );
-      },
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'search result',
+              style: Styles.textStyle18,
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Expanded(child: SearchResultListView())
+        ],
+      ),
     );
   }
 }
